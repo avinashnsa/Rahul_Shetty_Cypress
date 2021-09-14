6e230c462d00a4fdb3e90767184bf5b57fe9695e
Avinash <avinashnsa89@gmail.com>
2021-09-14 21:51:20 +0530

    testing1

--- cypress/integration/Atest.js
+++ cypress/integration/Atest.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-describe('My First Suite',function()
+describe('My First Suite testing branch',function()
 {
     before(()=>{
         cy.visit('https://qaboxletstestcypresspracticesite.netlify.app/differentdropdowntypes')
